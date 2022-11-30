0c5a971c6877e3ada1366a2840152fcd7d7c4ce7
Avinash Girme <82309854+AvinashGirme@users.noreply.github.com>
2022-11-30 23:20:36 +0530

    updated mongodb code

--- sensor/configuration/mongo_db_connection.py
+++ sensor/configuration/mongo_db_connection.py
@@ -10,8 +10,8 @@ class MongoDBClient:
     def __init__(self,database_name=DATABASE_NAME) -> None:
         try:
             if MongoDBClient.client is None:
-                #mongo_db_url = os.getenv(MONGODB_URL_KEY)
-                mongo_db_url="mongodb://localhost:27017/"
+                mongo_db_url = os.getenv(MONGODB_URL_KEY)
+                #mongo_db_url="mongodb://localhost:27017/"
                 print(mongo_db_url)
                 if "localhost" in mongo_db_url:
                     MongoDBClient.client=pymongo.MongoClient(mongo_db_url)
